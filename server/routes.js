a287491fd7358324951b4914789371a3aa5bcce9
Sean Tayler <seanjtayler@gmail.com>
2019-03-30 16:36:45 -0700

    updated routes

--- server/routes.js
+++ server/routes.js
@@ -1,28 +1,12 @@
 module.exports = function(app){
 
-    ////////////////// MASTER GET /////////////////////
-    app.get('/quotes', (req, res) => {
-      knex('quotes').where('user_id', users.id)
-
-  
-      res.json(data)
-    })
-
-
-    ////////////////// MASTER POST /////////////////////
-
     app.post('/quotes', (req, res) => {
         knex('quotes')
-          .where('user_id', users.id)
-          .where('location_id', locations.id)
           .insert({
             "quote": req.body.quote,
-            "size": req.body.size,
             "votes": req.body.votes,
             "points": req.body.points,
             "average": req.body.average,
-
-            "user_id": req.body.user_id,
           })
-        res.json(newPost)
+        res.json(newQuote)
       })
